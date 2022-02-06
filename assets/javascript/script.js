29c3af3f6909ab7f7b9a0bd0f8c40c8f8bf5f87c
ScottQuinn144 <scott.quinn144@outlook.com>
2022-02-06 09:06:08 +0000

    add new fuction to script.js add new button to index.html add style to button on style.css

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -5,26 +5,28 @@ function currentTime(){
     var minutes = date.getMinutes();
     var seconds = date.getSeconds();
     var amOrPm = 'AM';
+
+    if(hours == 0){
+        hours = 12;
+    }
+    
+    if(hours > 12){
+        hours = hours - 12;
+        session = "PM";
+    }
     
     hours = (hours < 10) ? "0" + hours : hours;
     minutes = (minutes < 10) ? "0" + minutes : minutes;
     seconds = (seconds < 10) ? "0" + seconds : seconds;
    
-     if (hours == 0){
-        hours = 12;
-        amOrPm = 'AM';
-    }
-      if (hours > 12){
-        hours = hours - 12;
-        amOrPm = 'PM';
-  
+    
      var time = hours + ":" + minutes + ":" + seconds + " " + amOrPm;
         document.getElementById("display").innerText = time;
         document.getElementById("display").textContent = time;
     
-    setTimeout(currentTime, 200);
+    setTimeout(currentTime, 1000);
     
-}}
+}
 
 function currentTimeZone(){
     let timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
@@ -34,3 +36,11 @@ function currentTimeZone(){
 
 currentTime();
 currentTimeZone();
+        
+function fullDate(){
+    var today = new Date();
+    var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+    document.getElementById("date").onclick = alert(date);
+    
+}
+
